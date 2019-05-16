909a4212f8c434717347e44a5eb5f273228ffed3
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-05-16 14:27:39 +0200

    fixing bookDetails component with stub

--- book-rating-ngrx/src/app/books/book-details/book-details.component.spec.ts
+++ book-rating-ngrx/src/app/books/book-details/book-details.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BookDetailsComponent } from './book-details.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BookStoreService } from '../shared/book-store.service';
+import { EMPTY } from 'rxjs';
 
 describe('BookDetailsComponent', () => {
   let component: BookDetailsComponent;
@@ -14,7 +15,9 @@ describe('BookDetailsComponent', () => {
       declarations: [BookDetailsComponent],
       providers: [{
         provide: BookStoreService,
-        useValue: undefined
+        useValue: {
+          getSingle: () => EMPTY
+        }
       }]
     })
     .compileComponents();
