80516a06802ecaa9e9b4f6bbb9ebac9c1a83ad98
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-05-16 15:28:33 +0200

    fixing test: providing an @imput()

--- book-rating-ngrx/src/app/books/book/book.component.spec.ts
+++ book-rating-ngrx/src/app/books/book/book.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookComponent } from './book.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('BookComponent', () => {
   let component: BookComponent;
@@ -8,6 +9,7 @@ describe('BookComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ BookComponent ]
     })
     .compileComponents();
@@ -16,6 +18,14 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+    component.book = {
+      isbn: '',
+      title: ':-)',
+      description: '',
+      rating: 1,
+      authors: [],
+      thumbnail: ''
+    };
     fixture.detectChanges();
   });
 
