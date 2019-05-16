db3f797c52d9ddcb54e21b1239aac8d8db61c50a
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-05-16 14:06:45 +0200

    fixing tests: appcomponent (trivial)

--- book-rating-ngrx/src/app/app.component.spec.ts
+++ book-rating-ngrx/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'book-rating'`, () => {
+  it(`should have as title 'Book Rating'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('book-rating');
+    expect(app.title).toEqual('Book Rating');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to book-rating!');
+    expect(compiled.querySelector('h1').textContent).toContain('Book Rating');
   });
 });
