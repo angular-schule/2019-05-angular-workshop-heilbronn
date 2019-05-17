ad7740d2c7581c92b2fb359ed54b2e8468ecfa6d
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-05-17 10:20:45 +0200

    hands on: rxjs

--- rxjs-playground-node/exercises/5_creation-functions.ts
+++ rxjs-playground-node/exercises/5_creation-functions.ts
@@ -1,2 +1,6 @@
 import { of, from, timer, interval } from 'rxjs';
 
+of('😍', '🙃').subscribe(console.log)
+
+
+from(['😍', '🙃']).subscribe(console.log)
--- rxjs-playground-node/exercises/6_operators.ts
+++ rxjs-playground-node/exercises/6_operators.ts
@@ -3,3 +3,14 @@ import { map, filter, reduce } from 'rxjs/operators';
 
 let dataArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+// Hands on:
+// mit 10 mulitplizieren
+// alle kleiner als 50 herausfiltern
+// die summe aus der übrig gebliebenen Zahlen
+
+from(dataArray).pipe(
+  map(x => x * 10)
+)
+
+
+.subscribe(console.log)
